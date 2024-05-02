c739707825a2401f98763a8a272f8f31a0503288
Niko2815 <146888012+Niko2815@users.noreply.github.com>
2024-05-02 10:34:51 -0500

    Update Rectangulo.java

--- src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Rectangulo.java
@@ -1,5 +1,20 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class Rectangulo {
-    
-}
+    private double lado1;
+    private double lado2;
+
+    public Rectangulo(double lado1, double lado2){
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }//cierra constructor
+
+    public double obtenerArea(){
+        return lado1 * lado2;
+    }//cierra obtenerArea
+
+    public double obtenerPerimetro(){
+        return (lado1*2)+(lado2*2);
+    }//cierra obtenerPerimetro
+
+}//cierra class
