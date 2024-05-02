b3579e41e72f6d8adbb4b787d6938e251c42df2d
Niko2815 <146888012+Niko2815@users.noreply.github.com>
2024-05-02 10:34:06 -0500

    Update FiguraGeometrica.java

--- src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
@@ -1,5 +1,38 @@
 package com.mycompany.figurasgeometricaspoo;
 
 public class FiguraGeometrica {
-    
-}
+    private String nombre;
+    private String color;
+
+    FiguraGeometrica (String nombre, String color){
+        this.nombre = nombre;
+        this.color = color;
+    }//cierra constructor
+
+    public double obtenerArea(){
+        
+        return 0;
+    }//cierra obtenerArea
+
+    public double obtenerPerimetro(){
+        
+        return 0;
+    }//cierra obtenerPerimetro
+
+    public String getNombre(){
+        return this.nombre;
+    }//cierra getNombre
+
+    public String getColor(){
+        return this.color;
+    }//cierra getColor
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }//cierra setNombre
+
+    public void setColor(String color){
+        this.color = color;
+    }//cierra setColor
+
+}//cierra class
