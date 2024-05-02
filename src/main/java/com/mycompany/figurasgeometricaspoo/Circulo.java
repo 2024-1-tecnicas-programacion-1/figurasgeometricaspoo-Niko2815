91c89de904264dcbddc44bd729537f9154e2ed10
Niko2815 <146888012+Niko2815@users.noreply.github.com>
2024-05-02 10:34:27 -0500

    Update Circulo.java

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,18 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Circulo {
-    
-}
+public class Circulo extends FiguraGeometrica {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }//cierra constructor
+
+    public double obtenerArea(){
+        return Math.PI * (radio*radio);
+    }//cierra obtenerArea
+
+    public double obtenerPerimetro(){
+        return 2*Math.PI*radio;
+    }//cierra obtenerPerimetro
+
+}//cierra class
